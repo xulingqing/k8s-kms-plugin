1150ca5bd304d320c02bec9ef011e6b76ae2a5ef
xulingqing <linggg@google.com>
2020-08-09 22:38:16 -0700

    modify test file - clean up unused func

--- cmd/k8s-kms-plugin/cmd/test.go
+++ cmd/k8s-kms-plugin/cmd/test.go
@@ -16,10 +16,8 @@ limitations under the License.
 package cmd
 
 import (
-	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
-	"crypto/x509/pkix"
 	"encoding/pem"
 	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
@@ -174,98 +172,7 @@ func runTest() error {
 		return err
 	}
 	logrus.Infof("Test 4 Returned LoadedSEK in PEM Format: %v", out)
-	/*
-		GenerateCAK
-	*/
-	logrus.Info("Test 5 GenerateCAK 4096 RSA")
-	var genCAKResp *istio.GenerateCAKResponse
-	if genCAKResp, err = c.GenerateCAK(ctx, &istio.GenerateCAKRequest{
-		Size:      4096,
-		Kind:      istio.KeyKind_RSA,
-		RootCaKid: cakKid,
-	}); err != nil {
-		logrus.Fatal(err)
-		return err
-	}
-
-	logrus.Infof("Test 5  GenerateCAK KID Returned: %s", string(genCAKResp.RootCaKid))
-	/*
-		GenerateCA
-	*/
-	logrus.Info("Test 6 GenerateCA, Sign and Store")
-	var genCAResp *istio.GenerateCAResponse
-	if genCAResp, err = c.GenerateCA(ctx, &istio.GenerateCARequest{
-
-		RootCaKid: cakKid,
-	}); err != nil {
-		logrus.Fatal(err)
-		return err
-	}
-	if debug {
-		out = string(genCAResp.Cert)
-	} else {
-		out = "Success"
-	}
-	logrus.Infof("Test 6  GenerateCA in : %s", out)
-
-	/*
-		SignCSR
-	*/
-	logrus.Info("Test 7 SignCSR Root CA Cert")
-	var signCSRResp *istio.SignCSRResponse
-	template := &x509.CertificateRequest{
-
-		SignatureAlgorithm: x509.SHA512WithRSA,
-		PublicKeyAlgorithm: x509.RSA,
-		Subject: pkix.Name{
-			CommonName: "Hello",
-		},
-		PublicKey: sek.Public(),
-		DNSNames:  []string{"awesome.com"},
-	}
-	req := &istio.SignCSRRequest{
-		RootCaKid: cakKid,
-	}
-
-	if req.Csr, err = x509.CreateCertificateRequest(rand.Reader, template, sek); err != nil {
-		return err
-	}
-	if signCSRResp, err = c.SignCSR(ctx, req); err != nil {
-		logrus.Fatal(err)
-		return err
-	}
-
-	logrus.Infof("Test 7 SignCSR Cert: %s", string(signCSRResp.Cert))
-
-	/*
-		DestroyCA
-	*/
-	logrus.Info("Test 8 DestroyCA")
-	var destroyCAResp *istio.DestroyCAResponse
-	if destroyCAResp, err = c.DestroyCA(ctx, &istio.DestroyCARequest{
-		KekKid: cakKid,
-	}); err != nil {
-		logrus.Fatal(err)
-		return err
-	}
-
-	logrus.Infof("Test 8 DestroyCA result : %b", destroyCAResp.Success)
-
-	/*
-		DestroyCAK
-	*/
-	logrus.Info("Test 9 DestroyCA")
-	var destroyCAKResp *istio.DestroyCAKResponse
-	if destroyCAKResp, err = c.DestroyCAK(ctx, &istio.DestroyCAKRequest{
-		KekKid: cakKid,
-	}); err != nil {
-		logrus.Fatal(err)
-		return err
-	}
-
-	logrus.Infof("Test 9 DestroyCAK result : %b", destroyCAKResp.Success)
-	logrus.Infof("------------------------------------------------------------")
-	return err
+	return nil
 
 }
 
